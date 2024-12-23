80c7968226d224e9398727a5df3f5c8d85255a88
Джохар <d.bubaev@academcity.online>
2024-12-23 23:24:27 +0300

    Реализовал генерацию и вывод URL адресов

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,76 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+const PORT = "8080"
+const DOMAIN = "http://localhost"
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	urls := make(map[string]string)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, func(res http.ResponseWriter, req *http.Request) {
+		if http.MethodPost != req.Method {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		responseData, err := io.ReadAll(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		body := string(responseData)
+
+		res.WriteHeader(http.StatusCreated)
+		res.Header().Set("content-type", "text/plain")
+		res.Header().Set("content-length", "30")
+
+		genId := RandStringBytes(8)
+		urls[genId] = body
+
+		url := fmt.Sprintf("%s:%s/%s", DOMAIN, PORT, genId)
+		res.Write([]byte(url))
+	})
+
+	mux.HandleFunc(`/{id}`, func(res http.ResponseWriter, req *http.Request) {
+		if http.MethodGet != req.Method {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		id := req.PathValue(`id`)
+
+		url, ok := urls[id]
+		if !ok {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		res.Header().Set("Location", url)
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	return http.ListenAndServe(`:`+PORT, mux)
+}
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bubaew95/yandex-go-learn
+
+go 1.23.4
