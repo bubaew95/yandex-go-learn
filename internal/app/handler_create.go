1a2ec459a4cc965c1c0be49e02d2228bafd20727
Джохар <d.bubaev@academcity.online>
2025-01-12 20:22:59 +0300

    Настроил динамический вывод content-length

--- internal/app/handler_create.go
+++ internal/app/handler_create.go
@@ -21,13 +21,13 @@ func (app *App) CreateURL(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.WriteHeader(http.StatusCreated)
-	res.Header().Set("content-type", "text/plain")
-	res.Header().Set("content-length", "30")
-
 	genID := utils.RandStringBytes(8)
 	app.URLs[genID] = body
-
 	url := fmt.Sprintf("%s/%s", app.Config.BaseURL, genID)
+
+	res.WriteHeader(http.StatusCreated)
+	res.Header().Set("content-type", "text/plain")
+	res.Header().Set("content-length", string(len(url)))
+
 	res.Write([]byte(url))
 }
