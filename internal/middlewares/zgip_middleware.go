091ffbf4ea716f8229f42250e12d202d2ffcd218
Джохар <d.bubaev@academcity.online>
2025-01-20 18:23:25 +0300

    update

--- internal/middlewares/zgip_middleware.go
+++ internal/middlewares/zgip_middleware.go
@@ -20,33 +20,33 @@ func GZipMiddleware(h http.Handler) http.Handler {
 		contentType := r.Header.Get("content-type")
 		_, accessContentType := accessContentTypes[contentType]
 
-		if accessContentType {
-			logger.Log.Info("accessContentTypes")
-			acceptEncoding := r.Header.Get("Accept-Encoding")
-			isSupportGZip := strings.Contains(acceptEncoding, "gzip")
-			if isSupportGZip {
-				logger.Log.Info("Accept-Encoding run")
+		// if accessContentType {
+		logger.Log.Info("accessContentTypes")
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		isSupportGZip := strings.Contains(acceptEncoding, "gzip")
+		if isSupportGZip {
+			logger.Log.Info("Accept-Encoding run")
 
-				cw := compresses.NewCompressWriter(w)
-				ow = cw
-				defer cw.Close()
-			}
-
-			contentEncoding := r.Header.Get("Content-Encoding")
-			isSendGZip := strings.Contains(contentEncoding, "gzip")
-			if isSendGZip {
-				logger.Log.Info("Content-Encoding run")
-
-				cr, err := compresses.NewCompressReader(r.Body)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				r.Body = cr
-				defer cr.Close()
-			}
+			cw := compresses.NewCompressWriter(w)
+			ow = cw
+			defer cw.Close()
 		}
+
+		contentEncoding := r.Header.Get("Content-Encoding")
+		isSendGZip := strings.Contains(contentEncoding, "gzip")
+		if isSendGZip {
+			logger.Log.Info("Content-Encoding run")
+
+			cr, err := compresses.NewCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			r.Body = cr
+			defer cr.Close()
+		}
+		// }
 
 		h.ServeHTTP(ow, r)
 	})
