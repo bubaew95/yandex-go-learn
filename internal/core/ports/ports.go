1eb55331f189e896b65fee4a328d7ffc8e78fd25
Джохар <d.bubaev@academcity.online>
2025-04-18 01:02:39 +0300

    update

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -11,7 +11,6 @@ type ShortenerRepository interface {
 	GetURLByOriginalURL(ctx context.Context, originalURL string) (string, bool)
 	SetURL(ctx context.Context, id string, url string) error
 	InsertURLs(ctx context.Context, urls []model.ShortenerURLMapping) error
-	InsertURLTwo(ctx context.Context, urls []model.ShortenerURLMapping) error
 	GetURLSByUserID(ctx context.Context, userID string) (map[string]string, error)
 	DeleteUserURLS(ctx context.Context, items []model.URLToDelete) error
 
