55529485db472cfcf421cdd864c1f69fce2508fc
Джохар <d.bubaev@academcity.online>
2025-04-23 00:54:36 +0300

    updated docs

--- internal/adapters/handlers/shortener_test.go
+++ internal/adapters/handlers/shortener_test.go
@@ -3,8 +3,6 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"github.com/bubaew95/yandex-go-learn/internal/core/ports"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -326,33 +324,4 @@ func TestHandlerBatch(t *testing.T) {
 			}
 		})
 	}
-}
-
-func ExampleShortenerHandler_AddNewURL() {
-	ctrl := gomock.NewController(nil)
-	defer ctrl.Finish()
-
-	mockService := ports.NewMockShortenerService(ctrl)
-	mockService.EXPECT().
-		GenerateURL(gomock.Any(), "https://example.com", gomock.Any()).
-		Return("http://short.url/abc123", nil)
-
-	handler := NewShortenerHandler(mockService)
-
-	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://example.com"))
-	w := httptest.NewRecorder()
-
-	handler.CreateURL(w, req)
-
-	res := w.Result()
-	defer res.Body.Close()
-
-	body, _ := io.ReadAll(res.Body)
-
-	fmt.Println(res.StatusCode)
-	fmt.Println(string(body))
-
-	// Output:
-	// 201
-	// http://short.url/abc123
 }
