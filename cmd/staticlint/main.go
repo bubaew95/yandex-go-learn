144d8cbd37e6d2e3fd8d7807636e6011f7f14340
Джохар <d.bubaev@academcity.online>
2025-05-13 17:20:39 +0300

    added analuse

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/bubaew95/yandex-go-learn/internal/analyzers/noosexit"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/ctrlflow"
+	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/analysis/passes/printf"
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/structtag"
@@ -31,11 +33,11 @@ func main() {
 		}
 	}
 
-	//// 4. Добавить два сторонних публичных анализатора (по выбору)
-	//analyzers = append(analyzers,
-	//	myPublicAnalyzer1.Analyzer,
-	//	myPublicAnalyzer2.Analyzer,
-	//)
+	// 4. Добавить два сторонних публичных анализатора (по выбору)
+	mychecks = append(mychecks,
+		inspect.Analyzer,
+		ctrlflow.Analyzer,
+	)
 
 	// 5. Добавить свой собственный
 	mychecks = append(mychecks, noosexit.NewAnalyzer())
