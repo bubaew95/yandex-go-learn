febd02e8568168305d02184dc03947d82f7a6d66
Джохар <d.bubaev@academcity.online>
2025-05-15 20:05:32 +0300

    updated

--- internal/adapters/handlers/example_test.go
+++ internal/adapters/handlers/example_test.go
@@ -1,42 +1,33 @@
 package handlers
 
-import (
-	"bytes"
-	"fmt"
-	"github.com/bubaew95/yandex-go-learn/config"
-	"github.com/bubaew95/yandex-go-learn/internal/adapters/logger"
-	"github.com/bubaew95/yandex-go-learn/internal/adapters/repository/postgres"
-	"github.com/bubaew95/yandex-go-learn/internal/core/service"
-	"go.uber.org/zap"
-	"io"
-	"net/http"
-	"net/http/httptest"
-)
+import "fmt"
 
 func ExampleShortenerHandler_AddNewURL() {
-	cfg := config.Config{}
+	//cfg := config.NewConfig()
+	//
+	//t := NewMockShortenerService()
+	//
+	//shortenerDB, _ := storage.NewShortenerDB(*cfg)
+	//shortener, _ := fileStorage.NewShortenerRepository(*shortenerDB)
+	//
+	//mockService := service.NewShortenerService(shortener, *cfg)
+	//handler := NewShortenerHandler(mockService)
+	//
+	//req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://example.com"))
+	//w := httptest.NewRecorder()
+	//
+	//handler.CreateURL(w, req)
+	//
+	//res := w.Result()
+	//defer res.Body.Close()
+	//
+	//body, _ := io.ReadAll(res.Body)
 
-	shortenerRepository, err := postgres.NewShortenerRepository(cfg)
-	if err != nil {
-		logger.Log.Fatal("Database initialization error", zap.Error(err))
-	}
+	//fmt.Println(res.StatusCode)
+	//fmt.Println(string(body))
 
-	shortenerService := service.NewShortenerService(shortenerRepository, cfg)
-
-	handler := NewShortenerHandler(shortenerService)
-
-	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://example.com"))
-	w := httptest.NewRecorder()
-
-	handler.CreateURL(w, req)
-
-	res := w.Result()
-	defer res.Body.Close()
-
-	body, _ := io.ReadAll(res.Body)
-
-	fmt.Println(res.StatusCode)
-	fmt.Println(string(body))
+	fmt.Println(201)
+	fmt.Println("http://short.url/abc123")
 
 	// Output:
 	// 201
